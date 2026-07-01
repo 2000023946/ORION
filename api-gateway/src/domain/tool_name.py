f9a8552c92154a03c4d4dd7cb9cc07224a060e4c
mo <mo@gmail.com>
2026-06-30 20:32:03 -0400

    added hash of tool name

--- api-gateway/src/domain/tool_name.py
+++ api-gateway/src/domain/tool_name.py
@@ -1,5 +1,13 @@
 from dataclasses import dataclass
 
-@dataclass 
+@dataclass(frozen=True)
 class ToolName:
     name: str
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __eq__(self, other):
+        if not isinstance(other, ToolName):
+            return False
+        return self.name == other.name
