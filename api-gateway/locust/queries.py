7f808e67db5c42aa3158aa43d90fcb0f255e5fe8
mo <mo@gmail.com>
2026-07-14 00:52:24 -0400

    added the locust

---
+++ api-gateway/locust/queries.py
@@ -0,0 +1,6 @@
+SEARCH_QUERIES = [
+    "find iphone products under 1000 dollars",
+    "search stock market information",
+    "find documents about distributed systems",
+    "find metadata about company projects",
+]
